f628538d3481e1b9a544fa7d69e4d7b6a9ea4423
Chal13W1zz <charles.muthii@student.moringaschool.com>
2022-04-17 16:58:15 +0300

    Add a homepage route

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/", (request, response)->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
